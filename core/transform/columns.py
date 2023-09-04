7eb2985a080b2023f84e4f95f1092cbb13635157
Frank Cara <frank.cara@picpay.com>
2023-09-04 17:23:00 -0400

    partitionby

--- core/transform/columns.py
+++ core/transform/columns.py
@@ -2,9 +2,14 @@ from pyspark.sql.functions import mean, stddev, max, min, sum, count, col, randn
 from pyspark.sql.functions import when, coalesce
 
 def withName(df, name):
-    if "name" in (col for col in df.columns):
+    columns =  (col for col in df.columns)
+    if "name" in columns:
         return df
-    elif "former_event_name" in (col for col in df.columns):
+    elif "former_event_name" in columns:
         return df.withColumn('name', col('former_event_name'))
+    elif "event" in columns:
+        return df.withColumn('name', col('event'))
+    elif "event_name" in columns:
+        return df.withColumn('name', col('event_name'))    
     return df.withColumn('name', lit(name))
 

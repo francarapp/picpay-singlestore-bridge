fded044237fb0d6e898e13714a86e26b05a7eb6d
frank.cara <frank.cara@picpay.com>
2023-09-04 17:12:55 +0000

    doers

---
+++ bridge-do-track.py
@@ -0,0 +1,24 @@
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+
+from core import session
+from bridge import Bridge
+from datetime import datetime, timedelta
+
+import logging
+
+def conf():
+    logging.basicConfig(format='[%(levelname)s] %(asctime)s {%(module)s} - %(message)s', level=logging.INFO)
+    logging.getLogger("core.extract.stream").setLevel(logging.DEBUG)
+    logging.getLogger('pyspark').setLevel(logging.ERROR)
+    logging.getLogger("py4j").setLevel(logging.ERROR)
+
+def main():
+    stream = Bridge("track", "track")
+    stream = stream.start()
+    stream.awaitTermination()
+
+
+if __name__ == "__main__":
+    conf()
+    main()

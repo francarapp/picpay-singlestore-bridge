e2be50abb656bba4ee42c2e32ff10dbaefb1c737
Frank Cara <frank.cara@picpay.com>
2023-09-04 12:01:11 -0400

    houston

--- bridge-do-houston.py
+++ bridge-do-houston.py
@@ -6,10 +6,12 @@ from bridge import Bridge
 
 from datetime import datetime, timedelta
 import logging
-
 def conf():
-    logging.basicConfig(format='[%(levelname)s] %(asctime)s - %(message)s', level=logging.INFO)
-    
+    logging.basicConfig(format='[%(levelname)s] %(asctime)s {%(module)s} - %(message)s', level=logging.INFO)
+    logging.getLogger("core.extract.stream").setLevel(logging.DEBUG)
+    logging.getLogger('pyspark').setLevel(logging.ERROR)
+    logging.getLogger("py4j").setLevel(logging.ERROR)
+     
 def main():
     stream = Bridge("houston", "houston")
     stream = stream.start()

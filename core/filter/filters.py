534649bfd72a241e09925d54b30b972d534b6366
Frank Cara <frank.cara@picpay.com>
2023-09-04 11:32:37 -0400

    length

--- core/filter/filters.py
+++ core/filter/filters.py
@@ -1,14 +1,19 @@
-from pyspark.sql.functions import col
+from pyspark.sql.functions import col, length
 
 def Filter(df):
     return filterYear(
+        filterLength(
             filterNull( 
                 filterNull(df, "user_id"), 
             "event_id"), 
-        "2023")
+        "properties", 10000000),
+    "2023")
 
 def filterYear(df, year):
     return df.filter(col('ano') >= year)
 
 def filterNull(df, column):
     return df.filter(col(column).isNotNull())
+
+def filterLength(df, column, size=10000000):
+    return df.filter(length(col(column)) <= size)

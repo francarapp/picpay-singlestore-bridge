2c7ef7b9437f9fac6d973d11e0f31968565731a7
frank.cara <frank.cara@picpay.com>
2023-09-04 15:10:01 +0000

    Filter

--- bridge.py
+++ bridge.py
@@ -12,8 +12,10 @@ log = logging.getLogger(__name__)
 def Bridge(landing, singlestore):
     source = f's3a://picpay-datalake-stream-landing/sparkstreaming/et/raw/{landing}-events-approved/'
     log.info(f"Bridging from {source} to {singlestore}")
-    stream = Filter(Shape(
-        Stream(source)
-    ), "2023")
+    stream = Filter(
+        Shape(
+            Stream(source)
+        )
+    )
     
     return SinkToSS(stream, singlestore)

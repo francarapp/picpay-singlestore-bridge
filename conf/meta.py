e066051a64a456fad90849e7f22eba4ed4b36c7c
Frank Cara <frank.cara@picpay.com>
2023-09-04 17:07:41 -0400

    partitionby

---
+++ conf/meta.py
@@ -0,0 +1,18 @@
+
+Meta = {
+    "houston" : {
+        "partitions": ["event", "year", "month", "day"]
+    },
+    "track" : {
+        "partitions": ["event", "year", "month", "day"]
+    },
+    "page" : {
+        "partitions": ["year", "month", "day"]
+    }
+}
+
+def partitionby(event, values):
+    part = Meta[event]
+    if part is None:
+        return []
+    return "/".join([f"{e[0]}={e[1]}" for e in zip(part, values)])

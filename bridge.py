e95d06528adb32feb5cb9520431c974f32fb08c8
Frank Cara <frank.cara@picpay.com>
2023-09-03 23:59:46 -0400

    Bridge

--- bridge.py
+++ bridge.py
@@ -11,8 +11,8 @@ def Bridge(landing, singlestore, since):
     )
 
     stream = stream.writeStream\
-        .option("checkpointLocation", "/home/spark/checkpoint/{landing}")\
-        .option("path", "s3a://picpay-dataeng-singlestore-landing/events/houston")\
+        .option("checkpointLocation", f"/home/spark/checkpoint/{landing}")\
+        .option("path", f"s3a://picpay-dataeng-singlestore-landing/events/{singlestore}")\
         .partitionBy("ano","mes", "dia", "hora", "minuto")\
         .format("parquet")
     stream.start()

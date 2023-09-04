7e4c1c2636763c85e08fb5d2195647aea4c641dc
Frank Cara <frank.cara@picpay.com>
2023-09-04 03:52:46 -0400

    Bridge

--- bridge.py
+++ bridge.py
@@ -6,9 +6,9 @@ from core.extract import Stream, SinkToSS
 from core.transform import  Shape
 from core.filter import filterYear
 
-def Bridge(landing, singlestore, since):
+def Bridge(landing, singlestore):
     stream = filterYear(Shape(
-        Stream(f's3a://picpay-datalake-stream-landing/sparkstreaming/et/raw/{landing}-events-approved/', since)
+        Stream(f's3a://picpay-datalake-stream-landing/sparkstreaming/et/raw/{landing}-events-approved/')
     ), "2023")
     
     return SinkToSS(stream, singlestore)

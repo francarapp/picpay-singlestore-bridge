6fe20bce1ae0494e0bf273c6291766575394d695
Frank Cara <frank.cara@picpay.com>
2023-09-04 17:27:30 -0400

    logging

---
+++ conf/log.py
@@ -0,0 +1,6 @@
+import logging
+def initlog():
+    logging.basicConfig(format='[%(levelname)s] %(asctime)s {%(module)s} - %(message)s', level=logging.INFO)
+    logging.getLogger("core.extract.stream").setLevel(logging.DEBUG)
+    logging.getLogger('pyspark').setLevel(logging.ERROR)
+    logging.getLogger("py4j").setLevel(logging.ERROR)
